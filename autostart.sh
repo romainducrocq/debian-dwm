ca4632ed003c2a35af4b3ee3ca1203c9727a22ee
romainducrocq <romain.g.ducrocq@gmail.com>
2025-07-29 18:15:53 +0300

    bluetooth

--- autostart.sh
+++ autostart.sh
@@ -4,6 +4,7 @@ dbus-launch &
 pulseaudio --start &
 pasystray > /dev/null 2>&1 &
 nm-applet > /dev/null 2>&1 &
+blueman-applet > /dev/null 2>&1 &
 feh --bg-scale /opt/debian-dwm/background/background.* &
 picom -b &
 /opt/debian-dwm/dwmblocks/dwmblocks &
--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ sudo apt-get install -y bat gcc make nano neofetch tar tree unzip wget \
     libxcb1-dev libx11-xcb-dev libxcb-res0-dev libharfbuzz-dev \
     brightnessctl dbus dbus-x11 dmenu feh picom rofi slock xclip zip \
     alsa-tools alsa-utils pulseaudio pavucontrol pulseaudio-module-bluetooth pasystray \
-    wpasupplicant wireless-tools network-manager network-manager-gnome gnome-keyring
+    wpasupplicant wireless-tools network-manager network-manager-gnome gnome-keyring \
+    bluetooth bluez-firmware bluez blueman
 
 # install librewolf
 sudo apt-get update
@@ -16,18 +17,6 @@ sudo apt-get install -y extrepo
 sudo extrepo enable librewolf
 sudo apt-get update
 sudo apt-get install librewolf
-
-# install configs
-cp -v .xinitrc ${HOME}
-cp -v .gitconfig ${HOME}
-mkdir -p ${HOME}/.librewolf
-cp -v librewolf.overrides.cfg ${HOME}/.librewolf
-mkdir -p ${HOME}/.config/rofi
-cp -v config.rasi ${HOME}/.config/rofi
-sudo cp -v 30-touchpad.conf /etc/X11/xorg.conf.d
-sudo cp -v NetworkManager.conf /etc/NetworkManager
-sudo rm -v /etc/network/interfaces
-echo "export PATH=\"$PATH:/opt/bin\"" >> ~/.bashrc
 
 # install fonts
 sudo cp -rv fonts/Iosevka /usr/share/fonts
@@ -54,4 +43,19 @@ sudo systemctl enable wpa_supplicant.service
 sudo systemctl start NetworkManager.service
 sudo systemctl start wpa_supplicant.service
 sudo NetworkManager
-# sudo systemctl enable bluetooth.service
+sudo systemctl enable bluetooth.service
+sudo service bluetooth restart
+gsettings set org.blueman.plugins.powermanager auto-power-on false
+
+# config install
+cp -v .xinitrc ${HOME}
+cp -v .gitconfig ${HOME}
+mkdir -p ${HOME}/.librewolf
+cp -v librewolf.overrides.cfg ${HOME}/.librewolf
+mkdir -p ${HOME}/.config/rofi
+cp -v config.rasi ${HOME}/.config/rofi
+sudo cp -v 30-touchpad.conf /etc/X11/xorg.conf.d
+sudo cp -v NetworkManager.conf /etc/NetworkManager
+sudo rm -v /etc/network/interfaces
+sudo sed -i -e 's/^AutoEnable=true/AutoEnable=false/' /etc/bluetooth/main.conf
+echo "export PATH=\"$PATH:/opt/bin\"" >> ~/.bashrc
